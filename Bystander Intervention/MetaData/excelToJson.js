e74b95587298d35fb972c074bea32cb56619d0e2
Unknown <connortkd@gmail.com>
2018-11-19 02:54:47 -0500

    Got Meta-Data working fully
    Good news: This is now working fully. I even updated the excel sheet so it prevents you from entering duplicate data under title and ID. Now, the code will automatically update the created json file so it is just an array. Then I iterate over the entire array, creating a file for each scenario. The file name is based off of the ID (which is why I forced it to be unique). So Event 5 will have a file name "Scenario5.json". I left the old version in though, so if we wanted to use the array notation and just have one large file, we can.

--- Bystander Intervention/MetaData/excelToJson.js
+++ Bystander Intervention/MetaData/excelToJson.js
@@ -12,7 +12,7 @@ const result = excelToJson(
         },
         columnToKey:
         {
-            A: 'Title',
+            A: 'SceneID',
             B: 'Title',
             C: 'Risk',
             D: 'mapID',
@@ -21,14 +21,71 @@ const result = excelToJson(
         }
     });
 
-//result will be the object
-console.log(result);
+    //truncates some header information, so the Json is just an array.
+    var singleResult = result.Sheet1;
+    //console.log(test);
+
+    
+    console.log(singleResult.length);
+    let length = Math.ceil(singleResult.length);
+    let part;
+    console.log("Starting parse");
+    let i;
+    /*For loop will go through the updated file, and get each line. It will then output
+    each lineas its own json. 
+    NOTE: if it will be easier to just use one Json file, the commented out section is below. 
+    */ 
+    for(i = 0; i < length; i++)
+    {
+        //console.log(i);
+        //
+            part = singleResult[i];
+    
+
 var fs = require('fs');
 
-fs.writeFile('fullMeta.json', JSON.stringify(result), (err) => {
+//write the json to a json file titled "fullMeta.json". This will all end up on one line though,
+//so there will need to be another function call to transform that file into multiple json files
+//for easier usability. 
+fs.writeFile('Scenario' + part.SceneID+'.json', JSON.stringify(part), (err) => {
+    if(err){
+        console.error(err);
+        return;
+    };
+    //console.log("Should be good");
+})
+
+
+        console.log(part);
+        console.log("NEW LINE");
+    }
+//result will be the object
+//console.log(result);
+//var fs = require('fs');
+
+//write the json to a json file titled "fullMeta.json". This will all end up on one line though,
+//so there will need to be another function call to transform that file into multiple json files
+//for easier usability. 
+/*fs.writeFile('fullMeta.json', JSON.stringify(test), (err) => {
     if(err){
         console.error(err);
         return;
     };
     console.log("Should be good");
 })
+*/
+
+
+
+/*Strucure:
+Use JSON.parse. 
+If needed, you may need top open the file and read it in. 
+We still have result actually, so this could work in our favor. 
+
+use something like 
+let sp
+
+
+
+*/
+

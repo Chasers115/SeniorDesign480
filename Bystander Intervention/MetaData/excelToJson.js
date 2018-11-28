6894135e3cbf260bda28c413f17d415384839366
Unknown <connortkd@gmail.com>
2018-11-28 09:23:36 -0500

    Updated Meta-Data
    Updated Meta-Data implementation a little bit. Added a few columns on the Excel sheet, more to see if it would work than for functional use. Added in an alternative version of metadata, where everything is in one json file as an array.

--- Bystander Intervention/MetaData/excelToJson.js
+++ Bystander Intervention/MetaData/excelToJson.js
@@ -2,11 +2,12 @@
 
 
 const excelToJson = require('convert-excel-to-json');
+var fs = require('fs');
 
 const result = excelToJson(
     {
         sourceFile: 'Book3.xlsx',
-        header: 
+        header:
         {
             rows: 1
         },
@@ -16,49 +17,51 @@ const result = excelToJson(
             B: 'Title',
             C: 'Risk',
             D: 'mapID',
-            E: 'EventID'
+            E: 'EventID',
+            F: 'Option One',
+            G: 'Option 2'
 
         }
     });
 
-    //truncates some header information, so the Json is just an array.
-    var singleResult = result.Sheet1;
-    //console.log(test);
+//truncates some header information, so the Json is just an array.
+var singleResult = result.Sheet1;
+//console.log(test);
+
+
+console.log(singleResult.length);
+let length = Math.ceil(singleResult.length);
+let part;
+console.log("Starting parse");
+let i;
+/*For loop will go through the updated file, and get each line. It will then output
+each lineas its own json. 
+NOTE: if it will be easier to just use one Json file, the commented out section is below. 
+*/
+
+for (i = 0; i < length; i++) {
+    //console.log(i);
+    //
+    part = singleResult[i];
+
 
     
-    console.log(singleResult.length);
-    let length = Math.ceil(singleResult.length);
-    let part;
-    console.log("Starting parse");
-    let i;
-    /*For loop will go through the updated file, and get each line. It will then output
-    each lineas its own json. 
-    NOTE: if it will be easier to just use one Json file, the commented out section is below. 
-    */ 
-    for(i = 0; i < length; i++)
-    {
-        //console.log(i);
-        //
-            part = singleResult[i];
-    
 
-var fs = require('fs');
-
-//write the json to a json file titled "fullMeta.json". This will all end up on one line though,
-//so there will need to be another function call to transform that file into multiple json files
-//for easier usability. 
-fs.writeFile('Scenario' + part.SceneID+'.json', JSON.stringify(part), (err) => {
-    if(err){
-        console.error(err);
-        return;
-    };
-    //console.log("Should be good");
-})
+    //write the json to a json file titled "fullMeta.json". This will all end up on one line though,
+    //so there will need to be another function call to transform that file into multiple json files
+    //for easier usability. 
+    fs.writeFile('Scenario' + part.SceneID + '.json', JSON.stringify(part), (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        };
+        //console.log("Should be good");
+    })
 
 
-        console.log(part);
-        console.log("NEW LINE");
-    }
+    console.log(part);
+    console.log("NEW LINE");
+}
 //result will be the object
 //console.log(result);
 //var fs = require('fs');
@@ -66,7 +69,7 @@ fs.writeFile('Scenario' + part.SceneID+'.json', JSON.stringify(part), (err) => {
 //write the json to a json file titled "fullMeta.json". This will all end up on one line though,
 //so there will need to be another function call to transform that file into multiple json files
 //for easier usability. 
-/*fs.writeFile('fullMeta.json', JSON.stringify(test), (err) => {
+/*fs.writeFile('fullMeta.json', JSON.stringify(singleResult), (err) => {
     if(err){
         console.error(err);
         return;
